e81bbb530126f27f947f247ea46760ddfa8dce26
jayan <jayanatl@gmail.com>
2019-07-04 04:35:07 -0400

    initial commit

---
+++ basic_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###############################################################################
+# Author: jayanatl@gmail.com
+# Description: Basic system check
+# Date: 2019-07-04
+###############################################################################
+
+# Variables
+hl="#####################################################################"
+
+# Functions
+ping_test() {
+    ping -c1 ${1} &> /dev/null && echo -en "\e[01;32m\xE2\x9C\x94 " || echo -en "\e[01;31m\xE2\x9D\x8C"; echo -e "$1\e[0m"
+}
+
+# Main
+echo ${hl}
+echo -e "HOSTNAME: \e[1;36m${HOSTNAME}\e[0m"
+echo ${hl}| tr '#' '-'
+
+echo "Testing network connectivity"
+echo ${hl:0:40}| tr '#' '.'
+nodes=(master node1 node2 node3 router google.com)
+for node in ${nodes[@]}; do
+    ping_test ${node}
+done
+echo ${hl}
