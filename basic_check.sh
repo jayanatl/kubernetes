01894fb115f7e16b3793f7687e7e630bc98b4043
Jayan <jayanatl@gmail.com>
2019-07-04 05:23:09 -0400

    update timeout

--- basic_check.sh
+++ basic_check.sh
@@ -10,7 +10,7 @@ hl="#####################################################################"
 
 # Functions
 ping_test() {
-    ping -c1 ${1} &> /dev/null && echo -en "\e[01;32m\xE2\x9C\x94 " || echo -en "\e[01;31m\xE2\x9D\x8C"; echo -e "$1\e[0m"
+    ping -c1 ${1} -W1 &> /dev/null && echo -en "\e[01;32m\xE2\x9C\x94 " || echo -en "\e[01;31m\xE2\x9D\x8C"; echo -e "$1\e[0m"
 }
 
 # Main
@@ -20,7 +20,7 @@ echo ${hl}| tr '#' '-'
 
 echo "Testing network connectivity"
 echo ${hl:0:40}| tr '#' '.'
-nodes=(master node1 node2 node3 router google.com)
+nodes=(master node1 node2 node3 router 1.1.1.1)
 for node in ${nodes[@]}; do
     ping_test ${node}
 done
